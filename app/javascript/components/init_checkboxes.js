1bc7eb22c815711495c91acdc06b9e1d50223798
Charlotte Caors <charlotte.caors@gmail.com>
2020-08-20 18:45:48 +0200

    css moods

--- app/javascript/components/init_checkboxes.js
+++ app/javascript/components/init_checkboxes.js
@@ -1,15 +1,11 @@
 const initCheckboxes = () => {
   const checkBoxes = document.querySelectorAll(".mood-checkbox")
-  console.log(checkBoxes);
   if (checkBoxes.length >= 1) {
     Array.from(checkBoxes).forEach(checkbox => {
       checkbox.addEventListener('click', (event) => {
-        console.log(event.currentTarget);
         event.currentTarget.classList.toggle('active');
         const checkBoxId = event.currentTarget.dataset.for;
-        console.log(checkBoxId);
         const hiddenCheckbox = document.querySelector(`#${checkBoxId}`);
-        console.log(hiddenCheckbox);
         if (hiddenCheckbox.checked) {
           hiddenCheckbox.checked = false;
         } else {
