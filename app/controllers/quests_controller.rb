428c2e96fd5c10f613a22e71245fb3da8b66c08f
Charlotte Caors <charlotte.caors@gmail.com>
2020-08-24 16:53:44 +0200

    progress done for flashcards

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -2,11 +2,21 @@ class QuestsController < ApplicationController
 
   def show
     @quest = Quest.find(params[:id])
+    @userquest = UserQuest.find_by(user: current_user, quest: @quest)
+    if @userquest.nil?
+      @userquest = UserQuest.create(user: current_user, quest: @quest)
+    end
   end
 
   def flashcards
     @quest = Quest.find(params[:quest_id])
-    @flashcards = @quest.flashcards
+    @userquest = UserQuest.find_by(user: current_user, quest: @quest)
+    @flashcards = @quest.flashcards.reject do |flashcard|
+      FlashcardsExecution.find_by(user_quest: @userquest, flashcard: flashcard)
+    end
+    @total_flashcards = @quest.flashcards.length
+    @done_flashcards_count = @total_flashcards - @flashcards.length
+    @progress = ((@done_flashcards_count/@total_flashcards.to_f)*100).truncate
   end
 
   def videos
---
+++ test/controllers/flashcards_execution_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FlashcardsExecutionControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
